88e444ea85726b725713dd6cb89cbe580baf6087
tanvi <10vidhate@gmail.com>
2018-06-14 21:54:57 -0700

    First Commit Login, SignUp and Home Screen

---
+++ app/src/main/java/edu/sdsu/tvidhate/hobo/helper/SharedConstants.java
@@ -0,0 +1,13 @@
+package edu.sdsu.tvidhate.hobo.helper;
+
+
+public interface SharedConstants {
+    String EMPTY_STRING = "";
+    String INVALID_EMAIL_PW_TOAST = "Invalid email or password";
+    String ENTER_EMAIL = "Enter Email ID";
+    String ENTER_PASSWORD = "Enter Password";
+    String PASSWORD_LENGTH = "Minimum length of Password is 6";
+    String INVALID_EMAIL = "Invalid Email ID";
+    String REGISTRATION_FAILED = "Registration failed";
+    String ENTER_REQUIRED_FIELDS = "Enter Required Fields";
+}
